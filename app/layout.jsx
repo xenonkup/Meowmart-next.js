fdea5e442463a240943c784f74c06582a8cc369d
Raynonkub <korakot.jorralee@gmail.com>
2025-06-19 21:47:22 +0700

    ลบ บรรทัดใน layout ที่ไม่ด้ใช้

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,39 +1,12 @@
-import { Geist, Geist_Mono } from "next/font/google";
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer"
-import { Toaster } from 'react-hot-toast';
 import "./style/globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
-export const metadata = {
-  title: "MeowMart",
-  description: "เว็บไซต์ขายของออนไลนฺสำหรับคนรักแมว",
-  icons: {
-    icon: "/assets/Logo/LogoMeowMart.png",
-  },
-};
 
 export default function RootLayout({ children }) {
   return (
-    <html suppressHydrationWarning>
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased min-h-screen flex flex-col`}
-      >
-       <Toaster />
-        <Navbar />
-        <main className="flex-1">
+    <html  lang="th">
+      <body>
+        <main>
           {children}
         </main>
-        <Footer />
       </body>
     </html>
   );
