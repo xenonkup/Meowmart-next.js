6771acd3df7d758461705bd84664af3c3aa55838
Raynonkub <korakot.jorralee@gmail.com>
2025-06-21 00:52:29 +0700

    ลบ classname main

--- app/(auth)/layout.jsx
+++ app/(auth)/layout.jsx
@@ -1,7 +1,7 @@
 export default function AuthLayout({ children }) {
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-100">
-      <main className="bg-white p-6 rounded shadow-md w-full max-w-md">
+      <main>
         {children}
       </main>
     </div>
