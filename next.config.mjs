0a459fbccbf49b2d71e1a5ad86de180af7ec0707
Raynonkub <korakot.jorralee@gmail.com>
2025-06-21 02:39:22 +0700

    เพิ่ม CSP Nextconfig

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,28 @@
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
 
-export default nextConfig
+  async headers() {
+    return [
+      {
+        source: '/(.*)', // ใช้กับทุกหน้าในเว็บไซต์
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: `
+              default-src 'self';
+              script-src 'self' 'unsafe-inline' 'unsafe-eval';
+              style-src 'self' 'unsafe-inline';
+              img-src 'self' data: https:;
+              font-src 'self';
+              connect-src 'self';
+              frame-src 'none';
+            `.replace(/\s{2,}/g, ' ').trim(),
+          },
+        ],
+      },
+    ];
+  },
+};
+
+export default nextConfig;
